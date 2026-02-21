647559b2434d92b44e008e320fb2bbd605144840
Rafi Khardalian <rmk@roobix.net>
2026-02-21 12:25:47 -0800

    Update memlimit to 1.1.1

--- Formula/memlimit.rb
+++ Formula/memlimit.rb
@@ -1,31 +1,16 @@
 class Memlimit < Formula
-\
   desc "Zero-dependency memory limiter using phys_footprint (macOS) and PSS (Linux)"
-\
   homepage "https://github.com/rmk40/memlimit"
-\
   url "https://github.com/rmk40/memlimit/archive/refs/tags/v1.1.1.tar.gz"
-\
   sha256 "f2136e3a2288335909c35460a1b6b5934d4582945d1a91eeeb6bf1d070f52cda"
-\
   license "MIT"
-\
 
-\
   def install
-\
     system "make", "install", "PREFIX=#{prefix}"
-\
   end
-\
 
-\
   test do
-\
     assert_match "memlimit #{version}", shell_output("#{bin}/memlimit --version")
-\
     system bin/"memlimit", "1G", "--", "true"
-\
   end
-\
 end
